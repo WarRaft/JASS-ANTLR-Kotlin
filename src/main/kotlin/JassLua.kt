7e78d96e3bd9e0bdceedd364b3f6d18fc1608e95
nazarpunk <nazarpunk@gmail.com>
2025-01-15 19:38:20 +0200

    0.0.1

--- src/main/kotlin/JassLua.kt
+++ src/main/kotlin/JassLua.kt
@@ -1,49 +1,29 @@
-@file:Suppress("DuplicatedCode")
-
 import raft.war.antlr.jass.JassState
 import raft.war.antlr.lua.JassLua
-import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.absolute
 
-fun j2l(path: Path): Path = path.resolveSibling(path.fileName.toString().replaceAfterLast('.', "lua"))
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("⚠️ No file provided")
+    }
 
+    val states: MutableList<JassState> = mutableListOf()
 
-fun t1() {
-    val cj = JassState()
-    val cjp = Paths.get("src", "test", "resources", "common.j")
-    cj.parse(cjp.absolute().toString())
-    JassLua(cj, j2l(cjp)).convert()
-    cj.errors.forEach { print("cj: $it \n") }
+    args.forEachIndexed { index, arg ->
+        println("read $arg")
 
-    val bj = JassState()
-    val bjp = Paths.get("src", "test", "resources", "blizzard.j")
-    bj.parse(bjp.absolute().toString(), listOf(cj))
-    JassLua(bj, j2l(bjp)).convert()
-    bj.errors.forEach { print("bj: $it \n") }
+        val s = JassState()
+        val p = Paths.get(arg)
 
-    val j = JassState()
-    val jp = Paths.get("src", "test", "resources", "test.j")
-    j.parse(jp.absolute().toString(), listOf(cj, bj))
-    JassLua(j, j2l(jp)).convert()
+        println("parse $arg")
+        s.parse(p.absolute().toString(), states)
+        states.add(s)
 
-    j.errors.forEach { print("j: $it \n") }
-}
+        s.errors.forEach { println("⚠️JASS error: $it") }
 
-fun t2() {
-    val j = JassState()
-    val jp = Paths.get("src", "test", "resources", "test.j")
-    j.parse(jp.absolute().toString(), listOf())
-    JassLua(j, j2l(jp)).convert()
-
-    j.errors.forEach { print("j: $it \n") }
-}
-
-
-fun main() {
-    if (true) {
-        t1()
-    } else {
-        t2()
+        val lp = p.resolveSibling(p.fileName.toString().replaceAfterLast('.', "lua"))
+        println("convert to $lp")
+        JassLua(s, lp).convert()
     }
 }
