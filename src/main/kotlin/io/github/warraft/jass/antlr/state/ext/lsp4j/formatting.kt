b0b0eeb68d9b3ef1cb82977921022961a33ebc40
nazarpunk <nazarpunk@gmail.com>
2025-02-22 17:43:37 +0200

    jass formatter begin

--- src/main/kotlin/io/github/warraft/jass/antlr/state/ext/lsp4j/formatting.kt
+++ src/main/kotlin/io/github/warraft/jass/antlr/state/ext/lsp4j/formatting.kt
@@ -3,13 +3,13 @@ package io.github.warraft.jass.antlr.state.ext.lsp4j
 import io.github.warraft.JassParser.*
 import io.github.warraft.jass.antlr.state.JassState
 import io.github.warraft.languages.lsp4j.LanguageServerEx
+import org.antlr.v4.runtime.ParserRuleContext
 import org.antlr.v4.runtime.Token
 import org.antlr.v4.runtime.tree.TerminalNode
 import org.eclipse.lsp4j.DocumentFormattingParams
 import org.eclipse.lsp4j.Position
 import org.eclipse.lsp4j.Range
 import org.eclipse.lsp4j.TextEdit
-
 
 private class Formatter(@Suppress("unused") val server: LanguageServerEx?, val root: RootContext) {
     val fmt = mutableListOf<TextEdit>()
@@ -19,28 +19,87 @@ private class Formatter(@Suppress("unused") val server: LanguageServerEx?, val r
         var t = ""
         repeat(num) { t += "\t" }
         val char = symbol.charPositionInLine
-        val p = num * 4
         val l = symbol.line - 1
-        if (char == p) return
+
+        //val p = num * 4
+        //if (char == p) return
         fmt.add(TextEdit(Range(Position(l, 0), Position(l, char)), t))
     }
 
-    fun tab(node: TerminalNode?, num: Int) {
-        tab(node?.symbol, num)
+    fun tab(node: TerminalNode?, num: Int) = tab(node?.symbol, num)
+    fun tab(ctx: ParserRuleContext?, num: Int) = tab(ctx?.start, num)
+
+    fun variable(ctx: VariableContext?) {
+        if (ctx == null) return
+        tab(ctx, 1)
+    }
+
+    fun stmt(list: List<StmtContext>?, level: Int) {
+        if (list == null) return
+        for (s in list) stmt(s, level)
+    }
+
+    fun stmt(ctx: ParserRuleContext?, level: Int) {
+        if (ctx == null) return
+        tab(ctx, level)
+        when (ctx) {
+            is StmtLoopContext -> {
+                stmt(ctx.stmt(), level + 1)
+                tab(ctx.ENDLOOP(), level)
+            }
+
+            is StmtIfContext -> {
+                tab(ctx.ENDIF(), level)
+                stmt(ctx.stmt(), level + 1)
+
+                for (elseIfCtx in ctx.elseif()) {
+                    tab(elseIfCtx, level)
+                    stmt(elseIfCtx.stmt(), level + 1)
+                }
+
+                val elseCtx: ElseRuleContext? = ctx.elseRule()
+                if (elseCtx != null) {
+                    tab(elseCtx, level)
+                    stmt(elseCtx.stmt(), level + 1)
+                }
+            }
+
+            is StmtCallContext,
+            is StmtSetContext,
+            is StmtExitWhenContext,
+            is StmtReturnContext,
+                -> null
+
+            else -> server?.log("stmt: ${ctx.javaClass.simpleName}")
+        }
+    }
+
+    fun expr() {
+
     }
 
     fun format() {
-        root.children.forEach {
-            when (it) {
+        for (ctx in root.children) {
+            when (ctx) {
                 is GlobalsContext -> {
-                    tab(it.GLOBALS(), 0)
-                    tab(it.ENDGLOBALS(), 0)
+                    tab(ctx.GLOBALS(), 0)
+                    tab(ctx.ENDGLOBALS(), 0)
+                    for (v in ctx.variable()) variable(v)
                 }
-                /*
-                is TypeContext -> typedef(it)
-                is NativeRuleContext -> JassFun.parse(it, null, this)
-                is FunctionContext -> JassFun.parse(it, null, this)
-                 */
+
+                is TypeContext -> {
+                    tab(ctx, 0)
+                }
+
+                is NativeRuleContext -> {
+                    tab(ctx, 0)
+                }
+
+                is FunctionContext -> {
+                    tab(ctx, 0)
+                    for (v in ctx.variable()) variable(v)
+                    stmt(ctx.stmt(), 1)
+                }
             }
         }
 
